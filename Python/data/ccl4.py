d2af27aa3afde4d1ce64fb45e18b45c1c362a405
HermannTheGreatMaster <hermann.boettcher@mail.de>
2018-12-03 16:41:46 +0100

    update

--- Python/data/ccl4.py
+++ Python/data/ccl4.py
@@ -5,8 +5,11 @@ from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 
 
-x0, y0 = np.loadtxt('CCl4_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
-x1, y1 = np.loadtxt('CCl4_pol1.txt', delimiter='\t', usecols=(0, 1), unpack=True)
+x0, y0 = np.loadtxt('CH2Cl2_pol1.txt', delimiter='\t', usecols=(0, 1), unpack=True)
+x1, y1 = np.loadtxt('CHCl3_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
+x2, y2 = np.loadtxt('CH2Cl2_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
 
+plt.plot(x1,y1)
+plt.plot(x2,y2)
 plt.plot(x0,y0)
 plt.show()
