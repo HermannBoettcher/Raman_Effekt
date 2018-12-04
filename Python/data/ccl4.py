3f8aa9b059f36897f3a3b1cb3778c2a015077a5d
HermannTheGreatMaster <hermann.boettcher@mail.de>
2018-12-04 15:06:27 +0100

    update

--- Python/data/ccl4.py
+++ Python/data/ccl4.py
@@ -5,11 +5,19 @@ from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 
 
-x0, y0 = np.loadtxt('CH2Cl2_pol1.txt', delimiter='\t', usecols=(0, 1), unpack=True)
-x1, y1 = np.loadtxt('CHCl3_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
-x2, y2 = np.loadtxt('CH2Cl2_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
-
-plt.plot(x1,y1)
-plt.plot(x2,y2)
+x0, y0 = np.loadtxt('Schwefel_pol0.txt', delimiter='\t', usecols=(0, 1), unpack=True)
+#x0=(x0*10**(2)+1/(532*10**(-9)))**(-1)*10**(9)
 plt.plot(x0,y0)
 plt.show()
+
+h=6.626070040*10**(-34)
+k= 1.38064852*10**(-23)
+n1=51426
+n2=6191
+l=532*10**(-9)
+c=299792458
+nu=470*10**2
+
+T=h*c*nu/(k*np.log((n1/n2)*((1/l-nu)**4/(1/l+nu)**4)))-273
+deltaT = h*c*nu/(k*np.log((n1/n2)*((1/l-nu)**4/(1/l+nu)**4)))**2*(1/((n1/n2)*((1/l-nu)**4/(1/l+nu)**4)))*4*((1/l-nu)**3/(1/l+nu)**3)
+print(T)
